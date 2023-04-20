94651d22b3a23ca75604e6a22cfdba67d90cc3db
Katherine Lantto <kathlantto@gmail.com>
2023-04-19 18:27:17 -0700

    began adding portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './portfolio.css';
-
+import
+import 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"></div>
+          <h3>This is a portoflio item title</h3>
+          <a href="https://github.com/" className='btn' target='_blank'>Github</a>
+          <a href="https://"className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
