c0b071b17fce2dbc526153a788f58abf466ff782
Katherine Lantto <kathlantto@gmail.com>
2023-04-20 12:19:20 -0700

    finishing up adding portfolio section styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,19 +1,99 @@
 import React from 'react'
 import './portfolio.css';
-import
-import 
+import DEPLOYEDPROJECT1 from '../../assets/deployedproject1.png'
+import MYTHICALMUTTS from '../../assets/mythicalmutts.png'
+import NOTES from '../../assets/notes.png'
+import PASSWORDGEN from '../../assets/passwordgen.png'
+import QUIZ from '../../assets/quiz.png'
+import TEAMPROFILE from '../../assets/teamprofile.png'
+import WEATHER from '../../assets/weather.png'
+import WORKDAYSCHEDULE from '../../assets/workdayschedule.png'
+
+const data = [
+  {
+    id: 1,
+    image: MYTHICALMUTTS,
+    title: 'Mythical Mutts Adoption Site',
+    github: "https://github.com/Azurelo/friendly-octo-palm-tree",
+    demo: "https://desolate-refuge-19752.herokuapp.com/"
+  },
+  {
+    id: 2,
+    image: DEPLOYEDPROJECT1,
+    title: 'Resplendent Recipe Finder',
+    github: "https://github.com/krchandler27/resplendent-recipe-finder",
+    demo: "https://krchandler27.github.io/resplendent-recipe-finder/"
+  },
+  {
+    id: 3,
+    image: NOTES,
+    title: 'Note Taker',
+    github: "https://github.com/krchandler27/resplendent-recipe-finder",
+    demo: "https://krchandler27.github.io/resplendent-recipe-finder/"
+  },
+  {
+    id: 4,
+    image: WEATHER,
+    title: 'Weather App',
+    github: "https://github.com/krchandler27/resplendent-recipe-finder",
+    demo: "https://krchandler27.github.io/resplendent-recipe-finder/"
+  },
+  {
+    id: 5,
+    image: WORKDAYSCHEDULE,
+    title: 'A Schedule For Your Day',
+    github: "https://github.com/krchandler27/resplendent-recipe-finder",
+    demo: "https://krchandler27.github.io/resplendent-recipe-finder/"
+  },
+  {
+    id: 6,
+    image: QUIZ,
+    title: 'Timed Quiz',
+    github: "https://github.com/krchandler27/resplendent-recipe-finder",
+    demo: "https://krchandler27.github.io/resplendent-recipe-finder/"
+  },
+  {
+    id: 7,
+    image: PASSWORDGEN,
+    title: 'Password Generator',
+    github: "https://github.com/krchandler27/resplendent-recipe-finder",
+    demo: "https://krchandler27.github.io/resplendent-recipe-finder/"
+  },
+  {
+    id: 8,
+    image: TEAMPROFILE,
+    title: 'Team Profile Generator',
+    github: "https://github.com/krchandler27/resplendent-recipe-finder",
+    demo: "https://krchandler27.github.io/resplendent-recipe-finder/"
+  },
+]
+
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
-          <h3>This is a portoflio item title</h3>
-          <a href="https://github.com/" className='btn' target='_blank'>Github</a>
-          <a href="https://"className='btn btn-primary' target='_blank'>Live Demo</a>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title}/>
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+              </div>
+            </article>
+            )
+          })
+        }
+
+
+
       </div>
     </section>
   )
